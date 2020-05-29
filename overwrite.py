5125f8b0d421bd90af36a977f51b04b263d7cc85
Jacob Neplokh <46184597+jneplokh@users.noreply.github.com>
2020-05-29 12:37:51 -0700

    Changed "container" to "volume"
    For consistency with Veracrypt terminology

--- overwrite.py
+++ overwrite.py
@@ -1,8 +1,8 @@
 import secrets
-veracrypt_container = input("What is the fullpath of the Veracrypt container?:\n")
+veracrypt_volume = input("What is the fullpath of the Veracrypt volume?:\n")
 random_bytes = secrets.token_bytes(2000000)
 random_byte_array = list(random_bytes)
-f = open(veracrypt_container, 'r+b')
+f = open(veracrypt_volume, 'r+b')
 f.seek(0)
 f.write(bytearray(random_byte_array))
 f.close()
