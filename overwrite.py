38a8394707229a51dbc1c035011fd73ec649bcb3
Jacob Neplokh <me@jacobneplokh.com>
2020-05-29 20:28:11 -0700

    Update script to overwrite all Veracrypt metadata
    The script now overwrites with random data everything until byyte 131072 and from S-131072 onwards as that is all of the non-data area.

--- overwrite.py
+++ overwrite.py
@@ -1,9 +1,20 @@
 import secrets
-veracrypt_volume = input("What is the fullpath of the Veracrypt volume?:\n")
-random_bytes = secrets.token_bytes(2000000)
+import os
+
+# Get veracrypt volume and size
+veracrypt_volume = input("What is the full path of the Veracrypt volume?:\n")
+volume_size = os.path.getsize(veracrypt_volume)
+
+# Overwrite all metadata until byte 131072 (data area, includes erasing hidden volume too)
+random_bytes = secrets.token_bytes(131072)
 random_byte_array = list(random_bytes)
 f = open(veracrypt_volume, 'r+b')
 f.seek(0)
 f.write(bytearray(random_byte_array))
-f.close()
 
+# Regenerate random bytes and overwrite all metadata from S-131072 onwards
+random_bytes = secrets.token_bytes(131072)
+random_byte_array = list(random_bytes)
+f.seek(int(volume_size - 131072))
+f.write(bytearray(random_byte_array))
+f.close()
