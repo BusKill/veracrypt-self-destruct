4039f9859b585fccce5f3f26ddb046929e753487
Jacob Neplokh <46184597+jneplokh@users.noreply.github.com>
2020-06-24 13:00:26 -0700

    Create admintest.py to test UAC elevation on Windows to eventually allow for OS overwrite script to run

---
+++ admintest.py
@@ -0,0 +1,11 @@
+################################################################################
+# File:    admintest.py
+# Purpose: Windows UAC Elevation Tester
+# Authors: Jacob Neplokh <me at jacobneplokh dot com>
+# Created: 2020-06-22
+# Updated: 2020-06-22
+# Version: 0.1
+#############################################################################
+
+import os
+import sys
