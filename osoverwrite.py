a63ad3285f55905a47d0d963579422677d78d37f
Jacob Neplokh <46184597+jneplokh@users.noreply.github.com>
2020-06-24 12:59:48 -0700

    Create osoverwrite.py for testing/creating OS overwrite script

---
+++ osoverwrite.py
@@ -0,0 +1,13 @@
+################################################################################
+# File:    osoverwrite.py
+# Purpose: Operating System Self-destruct trigger script for BusKill Kill Cord
+#          For more info, see: https://buskill.in/
+# WARNING: THIS IS EXPERIMENTAL SOFTWARE THAT IS DESIGNED TO CAUSE PERMANENT,
+#          COMPLETE AND IRREVERSIBLE DATA LOSS!
+# Authors: Jacob Neplokh <me at jacobneplokh dot com>
+# Created: 2020-06-01
+# Updated: 2020-06-01
+# Version: 0.1
+#############################################################################
+
+import os
