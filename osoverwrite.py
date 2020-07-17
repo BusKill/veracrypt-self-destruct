0749c4b2f39c72e63f90a086349646e12e21267f
Jacob Neplokh <me@jacobneplokh.com>
2020-07-16 22:18:02 -0700

    Move Windows Veracrypt command inside if statement and print message if user is on MacOS (no Veracrypt FDE on Mac)

--- osoverwrite.py
+++ osoverwrite.py
@@ -14,22 +14,28 @@
 import sys
 import os
 import secrets
-import ctypes
 import subprocess
 import platform
 
 
-# Check if User is Admin
+# Check Platforms
+CURRENT_PLATFORM = platform.system().upper()
+#if CURRENT_PLATFORM.startswith( 'LINUX' ):
+
+if CURRENT_PLATFORM.startswith( 'WIN' ):
+    import ctypes
+    if is_admin():
+        # Run VeraCrypt Command to force dismount all volumes, wipes cached passwords, and quit without user prompts
+        subprocess.call('"C:\Program Files\VeraCrypt\VeraCrypt.exe" /d /f /w /q /s', shell=True)
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+if CURRENT_PLATFORM.startswith( 'DARWIN' ):
+    print('MacOS is currently not supported as it cannot use Veracrypt FDE, a FileVault needs to be created.')
+    sys.exit()
+
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
-
-# If so, run rest of code. If not, request permissions.
-if is_admin():
-    # Run VeraCrypt Command to force dismount all volumes, wipes cached passwords, and quit without user prompts
-    subprocess.call('"C:\Program Files\VeraCrypt\VeraCrypt.exe" /d /f /w /q /s', shell=True)
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
